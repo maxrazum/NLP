91089797c3f166fd540e05afdb29c3d5d606a64e
maxrazum <75961644+maxrazum@users.noreply.github.com>
2021-04-12 10:27:16 -0300

    Added output in dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -24,6 +24,8 @@ module.exports = {
         ]
     },
     output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
         libraryTarget: 'var',
         library: 'Client'
     },
